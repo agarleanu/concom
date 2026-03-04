2fce3a1431ab4a35864800020b246c3a5218f6ec
Alexandru-Ștefan Gârleanu <agarleanu@proton.me>
2026-03-04 18:02:22 +0200

    fix: :adhesive_bandage: Move cursor to top after clearing

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,13 @@ use std::io::stdout;
 
 use concom::steps::run;
 use crossterm::{
+    cursor::MoveTo,
     execute,
     terminal::{Clear, ClearType},
 };
 
 fn main() -> std::io::Result<()> {
-    execute!(stdout(), Clear(ClearType::All))?;
+    execute!(stdout(), Clear(ClearType::All), MoveTo(0, 0))?;
 
     if let Err(err) = run() {
         eprintln!("{}", err);
